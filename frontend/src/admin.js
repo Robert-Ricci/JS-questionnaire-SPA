d9affba1c46bc241d4f6804fe061a4ee6cd97427
rmricci121 <rmricci121@gmail.com>
2020-07-26 05:45:38 -0400

    setup my js classes

---
+++ frontend/src/admin.js
@@ -0,0 +1,9 @@
+class Admin {
+    constructor(name, username, email) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+    }
+}
+
+//Allow teacher to access tests and view results
---
+++ frontend/src/answer.js
@@ -0,0 +1,6 @@
+class Answer {
+    constructor(name){
+        this.name = name;
+    }
+
+}
